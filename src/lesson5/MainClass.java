34d415449774ede37ec92a861e38f390951f705c
d.kudryashov <d.kudryashov2014@yandex.ru>
2018-12-05 00:20:27 +0500

    Lesson 05 homework

--- src/lesson5/MainClass.java
+++ src/lesson5/MainClass.java
@@ -3,7 +3,10 @@ package lesson5;
 public class MainClass {
 //    Object mon = new Object();
 
-    public static void main(String[] args) {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+    public static void main(String[] args) throws InterruptedException {
 //        MyThread t1 = new MyThread();
 //        MyThread t2 = new MyThread();
 //        t1.start();
@@ -147,7 +150,64 @@ public class MainClass {
 //            }
 //            System.out.println("Synch block end");
 //        }
+        countInOneThread();
+        countInTwoThreads();
+    }
 
+    public static void countInOneThread() {
+
+        float[] arr = getArray();
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long finish = System.currentTimeMillis();
+
+        System.out.println("Count in one thread took " + (finish - start) + " ms.");
+    }
+
+    public static void countInTwoThreads() throws InterruptedException {
+
+        float[] arr = getArray();
+
+        long start = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        System.arraycopy(arr,0, a1,0, h);
+        System.arraycopy(arr, h, a2,0, h);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        long finish = System.currentTimeMillis();
+
+        System.out.println("Count in two threads took " + (finish - start) + " ms.");
+    }
+
+    public static float[] getArray() {
+        float[] arr = new float[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        return arr;
     }
 }
 
