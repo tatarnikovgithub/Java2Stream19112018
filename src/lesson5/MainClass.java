975bcd13042d6b32d1d6e123173e8d44c59abf3e
smirnov.ae <smirnov.ae>
2018-12-06 15:10:13 +0500

    Added methods for estimate mulithreading timing

--- src/lesson5/MainClass.java
+++ src/lesson5/MainClass.java
@@ -1,153 +1,70 @@
 package lesson5;
 
 public class MainClass {
-//    Object mon = new Object();
+    static final int size = 10000000;
+    static final int h = size / 2;
 
-    public static void main(String[] args) {
-//        MyThread t1 = new MyThread();
-//        MyThread t2 = new MyThread();
-//        t1.start();
-//        t2.start();
-//
-//        Thread t3 = new Thread(new MyRunnable());
-//        Thread t4 = new Thread(new MyRunnable());
-//        t3.start();
-//        t4.start();
+    public static void main(String[] args) throws InterruptedException {
+        calculateArrValuesByOneThread();
+        calculateArrValuesByTwoThread();
+    }
 
-//        Thread t1 = new Thread(() -> System.out.println("A"));
-//        Thread t2 = new Thread(() -> System.out.println("B"));
-//        t1.start();
-//        t2.start();
+    private static void calculateArrValuesByOneThread() {
+        float[] arr = getFilledArr();
+        long start = System.currentTimeMillis();
 
-//        ExecutorService service = Executors.newFixedThreadPool(10);
-//        service.execute(() -> {
-//            System.out.println("A");
-//        });
-//        service.shutdown();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = calculateNewValue(i, arr[i]);
+        }
 
-//        new Thread(() -> {
-//            for (int i = 0; i < 10; i++) {
-//                System.out.println(i);
-//            }
-//        }).start();
+        long end = System.currentTimeMillis();
+        System.out.println("Calculate array values by one thread took " + (end - start) + " ms.");
+    }
 
-//        Thread t = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("A");
-//            }
-//        });
-//        t.setPriority(5);
-//        t.start();
+    private static void calculateArrValuesByTwoThread() throws InterruptedException {
+        float[] arr = getFilledArr();
+        long start = System.currentTimeMillis();
 
-//        Thread t = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 5; i++) {
-//                    System.out.println(i);
-//                }
-//            };
-//        });
-//        t.start();
-//        try {
-//            t.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("end");
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
 
-//        Thread t = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                int time = 0;
-//                while (true){
-//                    time++;
-//                    try {
-//                        Thread.sleep(1000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                    System.out.println(time);
-//                }
-//            }
-//        });
-//        t.setDaemon(true);
-//        t.start();
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("end");
-//        Counter counter = new Counter();
-//        Thread t1 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 100; i++) {
-//                    counter.inc();
-//                    try {
-//                        Thread.sleep(1);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        });
-//
-//        Thread t2 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 100; i++) {
-//                    counter.dec();
-//                    try {
-//                        Thread.sleep(1);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        });
-//
-//        t1.start();
-//        t2.start();
-//
-//        try {
-//            t1.join();
-//            t2.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println(counter.value());
-//        MainClass mc = new MainClass();
-//        new Thread(() -> {mc.method1();}).start();
-//        new Thread(() -> {mc.method1();}).start();
-//    }
-//
-//    public void method1() {
-//        System.out.println("Block1 begin");
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(i);
-//        try {
-//            Thread.sleep(100);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        }
-//        System.out.println("Block1 end");
-//        synchronized(mon){
-//            System.out.println("Synch block begin");
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(i);
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            }
-//            System.out.println("Synch block end");
-//        }
+        System.arraycopy(arr,0, a1,0, h);
+        System.arraycopy(arr, h, a2,0, h);
 
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = calculateNewValue(i, a1[i]);
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = calculateNewValue(i, a2[i]);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        long end = System.currentTimeMillis();
+        System.out.println("Calculate array values by two thread took " + (end - start) + " ms.");
+    }
+
+    private static float calculateNewValue(int i, float element) {
+        return (float)(element * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    }
+
+    private static float[] getFilledArr() {
+        float[] arr = new float[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        return arr;
     }
 }
 
